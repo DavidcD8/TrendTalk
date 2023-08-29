f037c9904f6d04319d862af5f621895aa8b05638
DavidcD8 <>
2023-08-29 09:54:45 +0000

    added home shortcut to navbar

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['trendtalk-010e54eb3de6.herokuapp.com',
-                 '8000-davidcd8-trendtalk-iils79uuma.us2.codeanyapp.com', '8000-davidcd8-trendtalk-ba8jlquisf.us2.codeanyapp.com']
+                 '8000-davidcd8-trendtalk-iils79uuma.us2.codeanyapp.com', '8000-davidcd8-trendtalk-ba8jlquisf.us2.codeanyapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-davidcd8-trendtalk-iils79uuma.us2.codeanyapp.com']
