224fa3886c0bfe0e80e0d9e1f8170bfb0b3eb725
DavidcD8 <>
2023-09-22 14:48:46 +0000

    fixed media queries for post iamge.

--- trendtalk/models.py
+++ trendtalk/models.py
@@ -3,11 +3,8 @@ from django.db.models.signals import post_save
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from PIL import Image
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
